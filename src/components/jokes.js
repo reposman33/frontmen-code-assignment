4317f990582592e5fd48ee9176c241c4c72445be
Marc Bakker <webdev@marcbakker.com>
2020-03-15 17:26:56 +0100

    Fixed selecting 10 random jokes from a list when there were already favouite jokes selected; adding onscreen text when random jokes selected; renaming 'favorite' to 'favourite'

--- src/components/jokes.js
+++ src/components/jokes.js
@@ -7,10 +7,11 @@ export class Jokes extends React.Component {
 	constructor(props) {
 		super(props);
 		const savedJokes = localStorage.getItem("jokes");
-		const savedFavoriteJokes = localStorage.getItem("favoriteJokes");
+		const savedfavouriteJokes = localStorage.getItem("favouriteJokes");
 		this.initialState = {
 			jokes: savedJokes ? JSON.parse(savedJokes) : [],
-			favoriteJokes: savedFavoriteJokes ? JSON.parse(savedFavoriteJokes) : []
+			favouriteJokes: savedfavouriteJokes ? JSON.parse(savedfavouriteJokes) : [],
+			feedback: ""
 		};
 		this.state = this.initialState;
 		this.api = new API();
@@ -18,36 +19,53 @@ export class Jokes extends React.Component {
 
 	componentDidMount() {}
 
-	onToggleFavorite = jokeId => {
-		const favorites = this.state.favoriteJokes.includes(jokeId)
-			? this.state.favoriteJokes.filter(id => id !== jokeId)
-			: [...this.state.favoriteJokes, jokeId];
-		this.setState({ favoriteJokes: favorites });
+	onTogglefavourite = jokeId => {
+		const favourites = this.state.favouriteJokes.includes(jokeId)
+			? this.state.favouriteJokes.filter(id => id !== jokeId)
+			: [...this.state.favouriteJokes, jokeId];
+		this.setState({ favouriteJokes: favourites });
 	};
 
 	// "We can also turn a timer on/off via a button (every 5 seconds). This will add one random joke to the
 	// favourites list http://api.icndb.com/jokes/random/1 until the list has 10 items.""
 	// vertaling: Via een knop kan elke 5 seconden een favoriete grap geselecteerd worden tot een maximum van 10.
-	selectRandomFavorite = () => {
-		// create 10 random favorite jokes;
+	selectRandomfavourite = () => {
+		const RANDOM_JOKES_COUNT = 10;
 		const randomJokes = [];
-		const jokes = this.state.jokes.slice();
-		for (let i = 0; i < 10; i++) {
-			let randomIndex = Math.floor(Math.random() * (jokes.length - 1));
-			randomJokes.push(jokes[randomIndex].id);
-			jokes.splice(randomIndex, 1);
+		// create 10 random favourite jokes that are not already favourites...
+		const jokesToChooseFrom = this.state.jokes.filter(joke => !this.state.favouriteJokes.includes(joke.id));
+		// are there enough jokes to randomly selectfrom?
+		const slack = jokesToChooseFrom.length - RANDOM_JOKES_COUNT;
+
+		// you cannot select 10 random items from a list containng 10 or less items
+		if (slack <= 0) {
+			alert(`Can not choose ${RANDOM_JOKES_COUNT} random jokes from a total of ${jokesToChooseFrom.length}`);
+			return;
 		}
-		// update the state every 5 secs with a favorite joke
+		// show user how many remaining jokes are selected
+		this.setState({ feedback: `Selecting 10 random jokes from a total of ${jokesToChooseFrom.length}` });
+		for (let i = 0; i < RANDOM_JOKES_COUNT; i++) {
+			// select a random joke
+			let randomIndex = Math.floor(Math.random() * jokesToChooseFrom.length);
+			randomJokes.push(jokesToChooseFrom[randomIndex].id);
+			// remove selected joke from source array - we don't want to select it again
+			jokesToChooseFrom.splice(randomIndex, 1);
+		}
+		// update the state every 5 secs with a favourite joke
 		const id = setInterval(() => {
-			this.setState({
-				favoriteJokes: [...this.state.favoriteJokes, randomJokes[0]]
-			});
+			// use functional setState or this will not work...
+			this.setState(() => ({
+				favouriteJokes: [...this.state.favouriteJokes, randomJokes[0]]
+			}));
+			// remove random favourite joke when displayed as selected
 			randomJokes.splice(0, 1);
-			if (randomJokes.length === 0) {
+			// stop showing random favourite jokes when there are no more
+			if (randomJokes.length <= 0) {
+				// remove feedback message
+				this.setState({ feedback: "" });
 				clearInterval(id);
 			}
-		}, 5000);
-		// add these to the state
+		}, 500);
 	};
 
 	onGetJokes = () => {
@@ -55,32 +73,41 @@ export class Jokes extends React.Component {
 	};
 
 	onSave = () => {
+		// if there are jokes, save in localStorage
 		this.state.jokes.length && localStorage.setItem("jokes", JSON.stringify(this.state.jokes));
-		this.state.favoriteJokes.length &&
-			localStorage.setItem("favoriteJokes", JSON.stringify(this.state.favoriteJokes));
+		// if there are favouriteJokes, save in localStorage
+		this.state.favouriteJokes.length &&
+			localStorage.setItem("favouriteJokes", JSON.stringify(this.state.favouriteJokes));
 	};
 
 	async getJokes() {
 		const jokes = await this.api.getRandomJokes(20);
-		this.setState({ jokes: jokes.map(joke => ({ id: joke.id, joke: joke.joke })) });
+		this.setState({ jokes: jokes.map(joke => ({ id: joke.id, joke: joke.joke })), favouriteJokes: [] });
 	}
 
 	render() {
 		return (
-			<div className='container'>
-				<h1>Click your favourite Chuck Norris Joke</h1>
-				<button onClick={this.onGetJokes}>More Jokes!</button>
-				<button onClick={this.onSave}>Save</button>
-				<button onClick={this.selectRandomFavorite}>Choose random</button>
-				{this.state.jokes.map(joke => (
-					<Joke
-						id={joke.id}
-						key={joke.id}
-						joke={joke.joke}
-						favorite={this.state.favoriteJokes.includes(joke.id)}
-						onClick={this.onToggleFavorite}></Joke>
-				))}
-			</div>
+			<React.Fragment>
+				<div className='header'>
+					<h1>Click to select your favourite Chuck Norris joke</h1>
+				</div>
+				<div className='container'>
+					{this.state.jokes.map(joke => (
+						<Joke
+							id={joke.id}
+							key={joke.id}
+							joke={joke.joke}
+							favourite={this.state.favouriteJokes.includes(joke.id)}
+							onClick={this.onTogglefavourite}></Joke>
+					))}
+				</div>
+				<div className='footer'>
+					<h3>{this.state.feedback.length ? this.state.feedback : null}</h3>
+					<button onClick={this.onGetJokes}>{this.state.jokes.length ? "Update" : "Show"} jokes</button>
+					<button onClick={this.onSave}>Save jokes</button>
+					<button onClick={this.selectRandomfavourite}>Choose 10 random jokes</button>
+				</div>
+			</React.Fragment>
 		);
 	}
 }
