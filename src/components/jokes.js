d7a3948f6d846d4c16cb55f787dffc34391b0a0f
Marc Bakker <webdev@marcbakker.com>
2020-03-15 11:49:06 +0100

    Make a favourite joke unfavorite by clicking it.

--- src/components/jokes.js
+++ src/components/jokes.js
@@ -18,11 +18,36 @@ export class Jokes extends React.Component {
 
 	componentDidMount() {}
 
-	onSetFavorite = jokeId => {
+	onToggleFavorite = jokeId => {
 		const favorites = this.state.favoriteJokes.includes(jokeId)
-			? this.state.favoriteJokes
+			? this.state.favoriteJokes.filter(id => id !== jokeId)
 			: [...this.state.favoriteJokes, jokeId];
 		this.setState({ favoriteJokes: favorites });
+	};
+
+	// "We can also turn a timer on/off via a button (every 5 seconds). This will add one random joke to the
+	// favourites list http://api.icndb.com/jokes/random/1 until the list has 10 items.""
+	// vertaling: Via een knop kan elke 5 seconden een favoriete grap geselecteerd worden tot een maximum van 10.
+	selectRandomFavorite = () => {
+		// create 10 random favorite jokes;
+		const randomJokes = [];
+		const jokes = this.state.jokes.slice();
+		for (let i = 0; i < 10; i++) {
+			let randomIndex = Math.random() * (jokes.length - 1);
+			randomJokes.push(jokes[randomIndex].id);
+			jokes.splice(randomIndex, 1);
+		}
+		// update the state every 5 secs with a favorite joke
+		const id = setInterval(() => {
+			this.setState({
+				favoriteJokes: [this.state.favoriteJokes, randomJokes[0]]
+			});
+			randomJokes.splice(0, 1);
+			if (id === 0) {
+				clearInterval(id);
+			}
+		}, 5000);
+		// add these to the state
 	};
 
 	onGetJokes = () => {
@@ -49,10 +74,12 @@ export class Jokes extends React.Component {
 						key={joke.id}
 						joke={joke.joke}
 						favorite={this.state.favoriteJokes.includes(joke.id)}
-						onClick={this.onSetFavorite}></Joke>
+						onClick={this.onToggleFavorite}></Joke>
 				))}
+				<h1>Click your favourite Chuck Norris Joke</h1>
 				<button onClick={this.onGetJokes}>More Jokes!</button>
 				<button onClick={this.onSave}>Save</button>
+				<button onClick={this.selectRandomFavorite}>Choose random</button>
 			</div>
 		);
 	}
